ece5c5f0f070798270ef5de820a2e9634c9b9389
ojide chibundo <chibundojuliet21@gmail.com>
2026-02-05 12:36:43 +0100

    real learning

--- tech.js
+++ tech.js
@@ -1,30 +1,41 @@
+
 let count = 0;
+
 const countDisplay = document.getElementById("count");
 const increaseBtn = document.getElementById("increase");
 const decreaseBtn = document.getElementById("decrease");
 const resetBtn = document.getElementById("reset");
 
+function updateColor() {
+  if (count > 0) {
+    countDisplay.style.color = "green";
+  } else if (count < 0) {
+    countDisplay.style.color = "blue";
+  } else {
+    countDisplay.style.color = "black";
+  }
+}
 
-// Step 3: Make functions for each button
 function increase() {
   count++;
   countDisplay.textContent = count;
+  updateColor();
 }
 
 function decrease() {
-  count--;
-  countDisplay.textContent = count;
-}
+    count--;
+    countDisplay.textContent = count;
+    updateColor();
+  }
+
 
 function reset() {
   count = 0;
   countDisplay.textContent = count;
+  updateColor();
 }
 
-// Step 4: Connect buttons to functions
 increaseBtn.addEventListener("click", increase);
 decreaseBtn.addEventListener("click", decrease);
 resetBtn.addEventListener("click", reset);
-
-
-
+countDisplay.style.color = "red";
